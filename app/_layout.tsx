a173070f8071621d30c83c4b536662d73a27f9bd
sarbanandabhikkhu <sarbanandabhikkhu@gmail.com>
2024-06-19 08:28:09 +0600

    initial setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,11 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -13,8 +13,8 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    Shurjo: require('../assets/fonts/Shurjo-Regular.ttf'),
-    Verajja: require('../assets/fonts/Verajja-Regular.ttf'),
+    Shurjo: require("../assets/fonts/Shurjo-Regular.ttf"),
+    Verajja: require("../assets/fonts/Verajja-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -28,7 +28,7 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
--- components/ThemedText.tsx
+++ components/ThemedText.tsx
@@ -14,7 +14,7 @@ export function ThemedText({ style, lightColor, darkColor, type = "default", ...
   return (
     <Text
       style={[
-        { color, fontFamily: "Shurjo" }, // Shurjo, Verajja
+        { color },
         type === "default" ? styles.default : undefined,
         type === "title" ? styles.title : undefined,
         type === "defaultSemiBold" ? styles.defaultSemiBold : undefined,
